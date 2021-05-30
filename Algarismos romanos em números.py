982b25ad5ce79e9fe03d83f62de7dda02fd5fb5e
victorhipolito <victorhipolito@hotmail.com.br>
2021-05-29 21:37:47 -0300

    Função de conversão de algarismos romanos para números (ainda não somados/subtraidos)

--- Algarismos romanos em números.py
+++ Algarismos romanos em números.py
@@ -1,6 +1,7 @@
 # Pedindo ao usuário um valor em algarísmos romanos para convertê-los para valores numéricos
 def alg_romanos(v):
     conv = [["I", 1],["V", 5],["X", 10],["L", 50],["C", 100],["D", 500],["M", 1000]]
+    #Array onde ficarão os números convertidos porém não somados/subtraídos
     numerico_ns = list()
     for letra in v:
         for corr in conv:
